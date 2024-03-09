d4e5e15387de8f51c6611c5ea84afabb3ee65d61
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2024-03-09 14:52:19 +0000

    variables learned

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId ="12345"
+let accountEmail = "mradul@gmail.com"
+var accountPassword = "password"
+let accountcity
+// value of accountId can't be changed as it is a const variable
+accountEmail= "tashu@gmail.com"
+accountPassword= "34567"
+accountcity="bengaluru"
+console.log(accountPassword)
+// prefer using let over var because of functional and block issues
+console.table([accountId,accountEmail,accountPassword,accountPassword])
