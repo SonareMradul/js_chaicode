6cf7526d99cea180c149531f2a93fcc0fe4f975c
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2024-03-09 15:31:42 +0000

    data types learned

---
+++ .devcontainer/01_basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict" // inform the compiler to use all js code according to newer version
+//alert(3+3) not valid as we are using node.js not browser
+console.log(3
+      +3) //this statement is valid but has very bad code readability so this way is not preferred
+console.log("Mradul")
+let name = "Mradul"
+let age = 21
+let isLogged = true
+let state
+// data types are started with capital letter in js except null & undefined because they are not proper data types
+// Number => 2 to the power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+// object
+console.log(typeof (undefined))
+console.log(typeof (null))
