8d20d79a004cedc0070c096b7d1551637c107818
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2024-03-11 13:51:43 +0000

    operators learned

---
+++ .devcontainer/01_basics/operators.js
@@ -0,0 +1,33 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+let str1 = "hello"
+let str2 = "Mradul"
+let str3 =  str1 + str2
+// console.log(str3) 
+// console.log("1" + 2) =>12
+// console.log(1+"2")   =>12
+// console.log("1"+ 2 +2) =>122
+// console.log(1+2+"2")     =>32
+//  console.log((3+4)*5%3) =>2 ( because multiplication operator has a higher
+//  precedence tha modulus operator)
+// console.log(+true) => result will be 1 as complier will see + opearator
+// and will treat true as number (1)
+// console.log(+"") => 0 will be ans as compiler will see empty string as zero
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
+
+
+
+
+
