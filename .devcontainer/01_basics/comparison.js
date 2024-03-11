050ceb5390224ab9c5e764611463972ab2e47f4e
SonareMradul <144688330+SonareMradul@users.noreply.github.com>
2024-03-11 15:01:30 +0000

    learned comparison operator

---
+++ .devcontainer/01_basics/comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log("2">1)
+// console.log("02">1) answer is true
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined ==0)
+console.log(undefined >0) //undefined is nothing just nothing
+console.log(undefined<0)
+//===
+console.log("2" === 2)
+
+
